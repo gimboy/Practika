25f6662acf31e7d9f21be21d4db223c5923a0ef4
Timur <gimboy09@mail.ru>
2022-10-20 18:59:54 +0300

    3

--- src/main/java/ArrayCalc.java
+++ src/main/java/ArrayCalc.java
@@ -1,8 +1,12 @@
-public class First {
+public class ArrayCalc {
     public static void main(String[] args) {
-        int[] ar1 = new int[6];
-        int[] ar2 = new int[5];
-        arrayDiff(ar1,ar2);
+//        int[] ar1 = new int[6];
+//        int[] ar2 = new int[5];
+//        arrayDiff(ar1,ar2);
+        int[] arr1 = {1,2,3,4,5};
+        int[] arr2 = {1,2,3,4,0};
+
+        arrayDivision(arr1,arr2);
     }
 
     static int[] arrayDiff(int[] arr1, int[] arr2) {
@@ -15,7 +19,15 @@ public class First {
         }
         return arr;
     }
+    static int[] arrayDivision(int[] arr1, int[] arr2) {
+        if (arr1 == null) throw new RuntimeException("arr1 = null");
+        if (arr2 == null) throw new RuntimeException("arr2 = null");
+        if (arr1.length != arr2.length) throw new RuntimeException("length arr1 != arr2");
+        int[] arr = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            if(arr2[i] == 0) throw new RuntimeException("division by 0");
+            arr[i] = arr1[i]/arr2[i];
+        }
+        return arr;
+    }
 }
-// 1. Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
-// и возвращающий новый массив, каждый элемент которого равен разности элементов двух входящих массивов в той же ячейке.
-// Если длины массивов не равны, необходимо как-то оповестить пользователя.
